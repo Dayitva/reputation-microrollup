38d803243797f8a69a9a64410358bf54898334a8
Dayitva <dayitvagoel@gmail.com>
2023-12-10 06:54:35 +0530

    fix: Token contract

--- contracts/src/VouchToken.sol
+++ contracts/src/VouchToken.sol
@@ -4,18 +4,27 @@ pragma solidity ^0.8.20;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC20Plugins } from "@1inch/token-plugins/contracts/ERC20Plugins.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VouchToken is ERC20Plugins {
+contract VouchToken is ERC20Plugins, Ownable {
+    address public owner;
     constructor(string memory name, string memory symbol, uint256 maxPluginsPerAccount, uint256 pluginCallGasLimit)
         ERC20(name, symbol)
         ERC20Plugins(maxPluginsPerAccount, pluginCallGasLimit)
-    {} // solhint-disable-line no-empty-blocks
+    {
+        owner = msg.sender;
+    } // solhint-disable-line no-empty-blocks
 
-    function mint(address account, uint256 amount) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner!");
+        _;
+    }
+
+    function mint(address account, uint256 amount) external onlyOwner {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) external {
+    function burn(address account, uint256 amount) external onlyOwner {
         _burn(account, amount);
     }
 }
